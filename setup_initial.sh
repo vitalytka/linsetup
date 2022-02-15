277faeaea3be7aeeabc28431d5cb9600a1bdd564
Vitaly Tkachuk <vitaly.tkachuk@inmarsatgov.com>
2022-02-15 15:41:34 -0500

    Make python3 default. Generate SSH keys.

--- setup_initial.sh
+++ setup_initial.sh
@@ -7,4 +7,6 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
 
-sudo apt install -y build-essential vim git htop tmux
+sudo apt install -y build-essential vim git htop tmux python-is-python3
+
+ssh-keygen -t rsa -b 4096
